82975f8f39804ccd7210e85bc9a90325e5054526
Gloriah-B <Gloriah-B@github.com>
2024-04-21 01:54:40 -0700

    0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This script creates a Flask web application with a single route.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+#defines the route for the root URL
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Display 'Hello HBNB!' when the root URL is accessed.
+    """
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    #start the flask development server
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/README.md
@@ -0,0 +1,11 @@
+0x04. AirBnB clone - Web framework
+
+A Web Framework
+How to build a web framework with Flask
+How to define routes in Flask
+A route
+How to handle variables in a route
+A template
+How to create a HTML response in Flask by using a template
+How to create a dynamic template (loops, conditions…)
+How to display in HTML data from a MySQL database
