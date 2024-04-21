b3db9634dc128419e7a8167587fd807aa8087ca4
Gloriah-B <Gloriah-B@github.com>
2024-04-21 05:02:33 -0700

    5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,76 +1,55 @@
 #!/usr/bin/python3
-"""
-This script creates a Flask web application with six routes.
+"""Start web application with two routings
 """
 
-from flask import Flask, render_template, abort
-from urllib.parse import unquote
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """
-    Display 'Hello HBNB!' when the root URL is accessed.
+@app.route('/')
+def hello():
+    """Return string when route queried
     """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """
-    Display 'HBNB' when /hbnb is accessed.
+@app.route('/hbnb')
+def hbnb():
+    """Return string when route queried
     """
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def display_c_text(text):
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Return reformatted text
     """
-    Display 'C ' followed by the value of the text variable.
-    Replace underscore _ symbols with a space.
-    """
-    text = unquote(text)  # Decode URL-encoded text
-    text = text.replace('_', ' ')  # Replace underscores with spaces
-    return 'C {}'.format(text)
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def display_python_text(text):
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_with_text(text='is cool'):
+    """Reformat text based on optional variable
     """
-    Display 'Python ' followed by the value of the text variable.
-    If no text is provided, use 'is cool' as the default.
-    """
-    text = unquote(text)  # Decode URL-encoded text
-    text = text.replace('_', ' ') if text else 'is cool'
-    return 'Python {}'.format(text)
+    return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def display_number(n):
+@app.route('/number/<int:n>')
+def number(n=None):
+    """Allow request if path variable is a valid integer
     """
-    Display 'n is a number' if n is an integer.
-    Otherwise, return a 404 error.
-    """
-    if isinstance(n, int):
-        return '{} is a number'.format(n)
-    else:
-        abort(404)
+    return str(n) + ' is a number'
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def display_number_template(n):
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """Retrieve template for request
     """
-    Display an HTML page with 'Number: n' if n is an integer.
-    Otherwise, return a 404 error.
-    """
-    if isinstance(n, int):
-        return render_template('number_template.html', number=n)
-    else:
-        abort(404)
+    path = '5-number.html'
+    return render_template(path, n=n)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
