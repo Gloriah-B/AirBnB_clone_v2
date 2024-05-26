82358e14403004f5e16e41d6e7d8072cd7b7d1de
Gloriah-B <Gloriah-B@github.com>
2024-05-26 13:04:08 -0700

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -45,9 +45,8 @@ def isanumber(n):
     """
     Display 'n is a number' if n is an integer.
     """
-    if isinstance(n, int):
     return "{:d} is a number".format(n)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
