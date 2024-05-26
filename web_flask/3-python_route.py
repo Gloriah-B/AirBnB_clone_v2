fc4e5bccdd9fca29712aa252740846b4cb408835
Gloriah-B <Gloriah-B@github.com>
2024-05-26 12:32:26 -0700

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,13 +4,11 @@ This script creates a Flask web application with four routes.
 """
 
 from flask import Flask
-from urllib.parse import unquote
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def index():
     """
     Display 'Hello HBNB!' when the root URL is accessed.
     """
@@ -18,7 +16,7 @@ def hello_hbnb():
 
 
 @app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
+def hbnb():
     """
     Display 'HBNB' when /hbnb is accessed.
     """
@@ -26,27 +24,22 @@ def display_hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def display_c_text(text):
+def c_text(text):
     """
     Display 'C ' followed by the value of the text variable.
-    Replace underscore _ symbols with a space.
     """
-    text = unquote(text)  # Decode URL-encoded text
-    text = text.replace('_', ' ')  # Replace underscores with spaces
-    return 'C {}'.format(text)
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def display_python_text(text):
+def python_text(text):
     """
     Display 'Python ' followed by the value of the text variable.
     Replace underscore _ symbols with a space.
     If no text is provided, use 'is cool' as the default.
     """
-    text = unquote(text)  # Decode URL-encoded text
-    text = text.replace('_', ' ') if text else 'is cool'
-    return 'Python {}'.format(text)
+    return 'Python ' + text.replace('_', ' ')
 
 
 if __name__ == "__main__":
